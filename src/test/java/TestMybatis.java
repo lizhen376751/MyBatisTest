45d3157ed4bde34724e9168727a308cef71ff7fb
724577807@qq.com <724577807@qq.com>
2018-04-21 22:17:27 +0800

    mybatis使用

--- src/test/java/TestMybatis.java
+++ src/test/java/TestMybatis.java
@@ -6,7 +6,9 @@ import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
-
+/**
+ * mybaitis使用
+ */
 public class TestMybatis {
 
 	public static void main(String[] args) {
